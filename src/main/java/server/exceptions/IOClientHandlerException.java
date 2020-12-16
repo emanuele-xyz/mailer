9778c5e66b160d380f8ec55549fc1a7dea84a78f
emanuele <me@emanuele.xyz>
2020-12-16 11:09:37 +0100

    remove unused exception

--- src/main/java/server/exceptions/IOClientHandlerException.java
+++
@@ -1,8 +0,0 @@
-package server.exceptions;
-
-public final class IOClientHandlerException extends Exception {
-
-    public IOClientHandlerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
