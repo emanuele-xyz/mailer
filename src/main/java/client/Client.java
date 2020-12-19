aa301b1bff6bf2ad2b955dbd38316bef0b610e96
emanuele <me@emanuele.xyz>
2020-12-19 09:41:21 +0100

    remove commented out code

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -41,47 +41,4 @@ public final class Client extends Application {
             e.printStackTrace();
         }
     }
-
-//    public static void main(String[] args) {
-//        try {
-//            String hostname = InetAddress.getLocalHost().getHostName();
-//            connectToServer(hostname);
-//        } catch (UnknownHostException e) {
-//            System.err.println(e.getMessage());
-//        }
-//    }
-//
-//    private static void connectToServer(String hostname) {
-//        try (Socket s = new Socket(hostname, Constants.SERVER_PORT);
-//             ObjectInputStream in = new ObjectInputStream(s.getInputStream());
-//             ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream())) {
-//
-//            System.out.println("Connected to server");
-//
-//            out.writeObject(Message.Login);
-//            out.writeObject("marco@mailer.xyz");
-//
-//            System.out.println("Sent LOGIN message to server");
-//
-//            try {
-//                Boolean response = Utils.read(Boolean.class, in);
-//                if (response == null) {
-//                    System.err.println("Error during login protocol: cannot read result as a Boolean");
-//                    return;
-//                }
-//
-//                if (response) {
-//                    System.out.println("Identified!");
-//                } else {
-//                    System.out.println("Not identified!");
-//                }
-//
-//            } catch (ClassNotFoundException e) {
-//                System.err.println(e.getMessage());
-//            }
-//
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
 }
