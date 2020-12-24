aa04671c42e4ff5dfaf4acd094347898376ee234
emanuele <me@emanuele.xyz>
2020-12-24 11:23:26 +0100

    fix not closing client socket if data stream creation fails

--- src/main/java/client/ServerDispatcher.java
+++ src/main/java/client/ServerDispatcher.java
@@ -8,6 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -38,7 +39,7 @@ public final class ServerDispatcher {
             socket.setSoTimeout(timeoutAfter);
 
             try {
-                // The socket will be closed in any case by our handler
+                // The socket will be closed by our handler if stream creation is successful
                 ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
                 ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
                 Callable<Message> task = new ServerHandler(socket, in, out, message);
@@ -49,10 +50,19 @@ public final class ServerDispatcher {
 
             } catch (IOException e) {
                 // Input or output stream creation
+
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+
                 e.printStackTrace();
             }
 
         } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
